1c9ba0751f1e40ba7cd2fc611ff05700b8ef47cb
abijithraaz <raazraaz801@gmail.com>
2021-08-06 10:27:33 +0530

    Update subreddit_twice_day.py
    change check.

--- subreddit_twice_day.py
+++ subreddit_twice_day.py
@@ -3,6 +3,7 @@
 import schedule
 import time
 import os
+#small change
   
 def job():
     os.system('python -m luigi --module subreddit_ranking_with_luigi MyTask --api-client-id 4IPibhzEWutTxA --api-client-secret ypLf67zPm1VCacoIvQlbcRBs65-Urw --local-scheduler')
